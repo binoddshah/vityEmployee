20e75e0ac6d817bb83f9e330d2c807df69a5c2da
binoddshah <binoddshah@gmail.com>
2024-01-17 19:05:49 +0545

    login initiated

---
+++ vityEmployeeInfoSystem/Controllers/accountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace vityEmployeeInfoSystem.Controllers
+{
+    public class accountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+
+    }
+}
